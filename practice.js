5dc1692cbef5551bfb355803b275fce761a43e83
Aakankshachaudhary <aakanksha352005@gmail.com>
2024-06-12 13:17:11 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,92 @@
+/*let age=10;
+console.log (age);*/
+
+/*let name1='it"s hello';
+console.log(name1);*/
+/*let ischeck=true
+console.log(ischeck);*/
+
+/*let person={"key":10}
+console.log(person["key"])*/
+/*let  person ={"key":10,"str":"hello","bool":true,"fun":function(){
+    return"obj"
+
+}}
+console.log(person["key"])
+console.log(person["str"])
+console.log(person["bool"])
+console.log(person["fun"])
+
+let arr=["hello",10]
+console.log(arr[0])*/
+/*function add(num){
+    /*console.log("my pocket money is "+(num+num)+" in current time");*/
+   /* console.log(`my pocket money is`+(num+num)+` in current time`);
+}
+
+add(100)*/
+/*let age=17;
+
+if (age>18) {
+    console.log("greater than 18");
+}
+else if (age==18) {
+
+    console.log("equal to 18");
+
+}
+else{
+    console.log("less than 18");
+}*/
+
+
+/*let str="monday";
+switch(str="monday"){
+    case"monday":
+    console.log("today is monday");
+    break;
+    case"tuesday":
+    console.log("today is tuesday");
+    break;
+    case"wednesday":
+    console.log("today is wednesday");
+    break;
+    default:
+        console.log("hindi week name");
+
+}*/
+/*let i=1;
+while(i<5){
+    console.log("hello");
+    i++;
+}*/
+/*let i=5;
+while(i>=0){
+    console.log("hello")
+    i--;
+}*/
+/*let i=1;
+while(i<=10){
+    console.log(i*2)
+    i++;
+}*/
+/*let i=1;
+do{
+    console.log(i*2);
+    i++;
+}while(i<=10);*/
+/*let i=0;
+for(i=0;i<5;i++){
+    console.log("hello")
+
+}-*/
+
+/*let table=2
+
+for(i=0;i<=10;i++){
+    
+    console.log(`${2}*${i}=${table*i}`)
+}*/
+/*let date = new Date();
+console.log(date.getDate());*/
+
